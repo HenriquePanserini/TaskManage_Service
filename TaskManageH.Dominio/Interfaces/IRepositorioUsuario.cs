5092ff3e74672f94a7825f7713bdeef3654d18a0
Henrique Panserini <henrique.panserini@gmail.com>
2024-09-17 17:38:40 -0300

    Adição das propriedades do repositorio na camada de infraesttrutura

--- TaskManageH.Dominio/Interfaces/IRepositorioUsuario.cs
+++ TaskManageH.Dominio/Interfaces/IRepositorioUsuario.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TaskManageH.Dominio.Entidades;
+using TaskManageH.Dominio.Interfaces.Base;
 
 namespace TaskManageH.Dominio.Interfaces
 {
-    internal interface IRepositorioUsuario
+    public interface IRepositorioUsuario : IRepositorioBase<Usuario> 
     {
+        Task<bool> AdicionaUsuario(string email, string senha, int idade, string celular);
+
+        Task<bool> ExisteUsuario(string email, string senha);
+
+        Task<bool> RetornaIdUsuario(string email);
     }
 }
